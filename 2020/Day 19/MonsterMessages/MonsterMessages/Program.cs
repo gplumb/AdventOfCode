74c6c9ef1dcd630732622a5dfc102ed51f661596
Graham Plumb <g.plumb@gmail.com>
2020-12-19 23:30:01 +0000

    Day 19 (pt 1)

---
+++ 2020/Day 19/MonsterMessages/MonsterMessages/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MonsterMessages
+{
+    class RuleEntry
+    {
+        public string Data { get; set; } = "";
+
+        public int RuleIndex { get; set; }
+
+        public bool IsData => !string.IsNullOrWhiteSpace(Data);
+
+        public RuleEntry(string item)
+        {
+            if(item.StartsWith("\""))
+            {
+                Data = item.Substring(1, item.Length - 2);
+                return;
+            }
+
+            RuleIndex = int.Parse(item);
+        }
+    }
+
+    class Data
+    {
+        public Dictionary<int, List<List<RuleEntry>>> Rules { get; } = new Dictionary<int, List<List<RuleEntry>>>();
+
+        public Dictionary<int, List<string>> Evaluated { get; } = new Dictionary<int, List<string>>();
+
+        public List<string> Messages { get; } = new List<string>();
+
+        public void EvaluateRules()
+        {
+            for (int x = 0; x < Rules.Count; x++)
+            {
+                Evaluated[x] = Evaluate(x);
+            }
+        }
+
+        public List<string> Evaluate(int index)
+        {
+            if (Evaluated.ContainsKey(index))
+                return Evaluated[index];
+
+            var rule = Rules[index];
+            var result = new List<string>();
+
+            // Iterate each entry in the rule
+            foreach (var entry in rule)
+            {
+                var list = new List<string>() { "" };
+
+                // Iterate each component in the rule
+                foreach (var component in entry)
+                {
+                    if (component.IsData)
+                    {
+                        return new List<string>() { component.Data };
+                    }
+
+                    if(!Evaluated.ContainsKey(component.RuleIndex))
+                        Evaluated[component.RuleIndex] = Evaluate(component.RuleIndex);
+
+                    var newList = new List<string>();
+
+                    for(int p = 0; p < list.Count; p++)
+                    {
+                        for(int b = 0; b < Evaluated[component.RuleIndex].Count; b++)
+                        {
+                            newList.Add(list[p] + Evaluated[component.RuleIndex][b]);
+                        }
+                    }
+
+                    list = newList;
+                }
+
+                result.AddRange(list);
+            }
+            
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Puzzle 1
+            //var data = LoadRulesAndMessages("TestData.txt");
+            var data = LoadRulesAndMessages("Input1.txt");
+            data.EvaluateRules();
+
+            var count = 0;
+            foreach(var item in data.Messages)
+            {
+                if (data.Evaluated[0].Any(x => x.Equals(item)))
+                    count++;                
+            }
+
+            Console.WriteLine($"Matches: {count}");
+        }
+
+        static Data LoadRulesAndMessages(string filename)
+        {
+            var reader = default(StreamReader);
+            var input = default(string);
+            var result = new Data();
+
+            var isRule = true;
+
+            try
+            {
+                using (reader = new StreamReader(filename))
+                {
+                    while ((input = reader.ReadLine()) != null)
+                    {
+                        if (input.Length == 0)
+                        {
+                            isRule = false;
+                            continue;
+                        }
+
+                        if(isRule)
+                        {
+                            var split = input.Split(new string[] { ":", "|" }, StringSplitOptions.RemoveEmptyEntries);
+                            var index = int.Parse(split[0]);
+                            var data = new List<List<RuleEntry>>();
+
+                            for (int x = 1; x < split.Length; x++)
+                            {
+                                var rules = new List<RuleEntry>();
+                                var numbers = split[x].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                                foreach (var item in numbers)
+                                {
+                                    rules.Add(new RuleEntry(item));
+                                }
+
+                                data.Add(rules);
+                            }
+
+                            result.Rules.Add(index, data);
+                            continue;
+                        }
+
+                        result.Messages.Add(input);
+                    }
+
+                    return result;
+                }
+            }
+            finally
+            {
+                reader?.Close();
+            }
+        }
+    }
+}
---
+++ 2020/Day 19/OperationOrder/OperationOrder/Program.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace OperationOrder
+{
+    class Program
+    {
+        enum Operation
+        {
+            Add,
+            Multiply
+        }
+
+        static void Main(string[] args)
+        {
+            //var expression = "1 + 2 * 3 + 4 * 5 + 6";                               // 71 or 231
+            //var expression = "1 + (2 * 3) + (4 * (5 + 6))";                         // 51
+            //var expression = "2 * 3 + (4 * 5)";                                     // 26 or 46
+            //var expression = "5 + (8 * 3 + 9 + 3 * 4 * 3)";                         // 437 or 1445
+            //var expression = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))";           // 12240 or 669060
+            //var expression = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2";     // 13632 or 23340
+            
+            //var result = EvaluateExpression(expression);
+            //var result = EvaluateAdvanced(expression);
+
+            var list = LoadExpressions("Input1.txt");
+            var sum = 0L;
+
+            foreach (var exp in list)
+            {
+                // Puzzle 1
+                //sum += EvaluateExpression(exp);
+
+                // Puzzle 2
+                sum += EvaluateAdvanced(exp);
+            }
+
+            Console.WriteLine($"Sum = {sum}");
+        }
+
+        private static List<string> Tokenzie(string text)
+        {
+            var tokens = new List<string>();
+            var snippet = new StringBuilder();
+            var bracketCount = 0;
+            var newToken = false;
+
+            foreach(var ch in text)
+            {
+                if (ch == ' ')
+                {
+                    if (bracketCount > 0)
+                    {
+                        snippet.Append(ch);
+                        continue;
+                    }
+
+                    newToken = true;
+                }
+                else if (ch == ')')
+                {
+                    snippet.Append(ch);
+                    bracketCount--;
+                }
+                else if (ch == '(')
+                {
+                    snippet.Append(ch);
+                    bracketCount++;
+                    continue;
+                }
+                else
+                {
+                    snippet.Append(ch);
+                    continue;
+                }
+
+                if(newToken)
+                {
+                    tokens.Add(snippet.ToString());
+                    snippet.Clear();
+                    newToken = false;
+                }
+            }
+
+            if(snippet.Length > 0)
+                tokens.Add(snippet.ToString());
+
+            return tokens;
+        }
+
+        static long EvaluateExpression(string expression)
+        {
+            var tokens = Tokenzie(expression);
+
+            long? lhs = null;
+            long? rhs = null;
+            var operation = Operation.Multiply;
+
+            foreach (var item in tokens)
+            {
+                switch (item)
+                {
+                    case "+":
+                        operation = Operation.Add;
+                        break;
+
+                    case "*":
+                        operation = Operation.Multiply;
+                        break;
+
+                    default:
+                        // Are we dealing with nesting?
+                        var value = 0L;
+
+                        if(item.StartsWith("("))
+                        {
+                            // Yes, strip the brackets and evaluate it
+                            var subExpr = item.Substring(1, item.Length - 2);
+                            value = EvaluateExpression(subExpr);
+                        }
+                        else
+                        {
+                            value = int.Parse(item);
+                        }
+
+                        if(!lhs.HasValue)
+                        {
+                            lhs = value;
+                        }
+                        else
+                        {
+                            rhs = value;
+                        }
+
+                        break;
+                }
+
+                if(lhs.HasValue && rhs.HasValue)
+                {
+                    switch (operation)
+                    {
+                        case Operation.Add:
+                            lhs = lhs + rhs;
+                            rhs = null;
+                            break;
+
+                        case Operation.Multiply:
+                            lhs = lhs * rhs;
+                            rhs = null;
+                            break;
+
+                        default:
+                            throw new NotImplementedException();
+                    }
+                }
+            }
+
+            return lhs.Value;
+        }
+
+        static long EvaluateAdvanced(string expression)
+        {
+            var tokens = Tokenzie(expression);
+            var reduced = new List<string>();
+
+            // Expand out all of the brackets
+            while (tokens.Any(x => x.StartsWith("(")))
+            {
+                foreach (var item in tokens)
+                {
+                    if (item.StartsWith("("))
+                    {
+                        // Yes, strip the brackets and evaluate it
+                        var subExpr = item.Substring(1, item.Length - 2);
+                        var value = EvaluateAdvanced(subExpr).ToString();
+                        reduced.Add(value);
+                    }
+                    else
+                    {
+                        reduced.Add(item);
+                    }
+                }
+
+                tokens = reduced;
+                // reduced.Clear();
+            }
+
+            // Now we cheat and put brackets around all of the plus operations to give it precedence
+            var builder = new StringBuilder("");
+
+            for(int x=0; x < tokens.Count; x++)
+            {
+                if(tokens[x] == "+")
+                {
+                    tokens[x - 1] = "(" + tokens[x - 1];
+                    tokens[x + 1] = tokens[x + 1] + ")";
+                }
+            }
+
+            var exp = string.Join(" ", tokens);
+            var result = EvaluateExpression(exp);
+            return result;
+        }
+
+        static List<string> LoadExpressions(string filename)
+        {
+            var reader = default(StreamReader);
+            var input = default(string);
+            var result = new List<string>();
+
+            try
+            {
+                using (reader = new StreamReader(filename))
+                {
+                    while ((input = reader.ReadLine()) != null)
+                        result.Add(input);
+
+                    return result;
+                }
+            }
+            finally
+            {
+                reader?.Close();
+            }
+        }
+    }
+}
